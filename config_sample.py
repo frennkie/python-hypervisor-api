9b9171a703f046c28840b3d739d984d2c621fb19
Robert Habermann <mail@rhab.de>
2017-01-15 10:09:46 +0100

    remove things I don't need

---
+++ config_sample.py
@@ -0,0 +1,3 @@
+ESXI_HOST = "192.168.0.100"
+ESXI_USER = "root"
+ESXI_PASS = "password"
---
+++ host-info.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import api_vmware_include
+import config
+
+def main():
+    # change these to match your installation
+    host = config.ESXI_HOST
+    user = config.ESXI_USER
+    pw = config.ESXI_PASS
+
+    #connect to the host
+    host_con = api_vmware_include.connectToHost(host, user, pw)
+
+    # list server type
+    print("Type: {}".format(host_con.get_server_type()))
+
+    print("Datacenters: {}".format(host_con.get_datacenters().items()))
+    print("Hosts: {}".format(host_con.get_hosts().items()))
+    print("Datastores: {}".format(host_con.get_datastores().items()))
+
+    # disconnect from the host
+    host_con.disconnect()
+
+if __name__ == '__main__':
+    main()
